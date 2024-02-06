53fdd996185b949469659b8cff84c69f6206f99d
yasoug <ougouisyassine@gmail.com>
2024-02-06 20:07:42 +0000

    task 2 : commit 1

---
+++ 0x02-i18n/2-app.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+"""basic babel setup"""
+
+from flask import Flask, render_template, request
+from flask_babel import Babel
+
+
+class Config:
+    """Configuration class for Flask application"""
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
+
+
+app = Flask(__name__)
+app.config.from_object(Config)
+babel = Babel(app)
+
+
+@babel.localeselector
+def get_locale():
+    """determine the best match with supported languages"""
+    return request.accept_languages.best_match(app.config["LANGUAGES"])
+
+
+@app.route("/")
+def basic():
+    """renders 2-index.html"""
+    return render_template("2-index.html")
